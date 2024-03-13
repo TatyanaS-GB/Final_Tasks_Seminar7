863f8fdfbecfd7726a935b065f0a9aabe51c55de
Tatyana Stasenko <sale_rim@mail.ru>
2024-03-13 23:02:16 +0700

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,39 +6,44 @@
 // M = 1; N = 5 -> ,"1, 2, 3, 4, 5"
 // M = 4; N = 8 -> ,"4, 5, 6, 7, 8"
 
-Console.WriteLine("Введите натуральное число M: ");
-int M = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите натуральное число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.Write($"M = {M}; N = {N} -> \"");
-
 void NaturalNumbers(int num1, int num2)
 {
     if (num1 == num2)
     {
-        if (num1 >= 0)
-            Console.Write($"{num1}");
+        if (num2 >= 0) Console.Write($"{num2}");
     }
     else
     {
         if (num1 < num2)
         {
             NaturalNumbers(num1, num2 - 1);
-
-            if (num2 >= 0)
-                Console.Write($"{", "}{num2, 2}");
+            if (num2 >= 0) Console.Write($", {num2}");
         }
         else if (num1 > num2)
         {
-            if (num2 >= 0)
-                Console.Write($"{", "}{num1, 2}");
+            if (num2 >= 0) Console.Write($"{num1}, ");
             NaturalNumbers(num1 - 1, num2);
+
         }
     }
-
-
 }
 
-NaturalNumbers(M, N);
-Console.Write("\"");
-
+Console.WriteLine("Введите натуральное число M: ");
+if (int.TryParse(Console.ReadLine(), out int M) && M > 0)
+{
+    Console.WriteLine("Введите натуральное число N: ");
+    if (int.TryParse(Console.ReadLine(), out int N) && N > 0)
+    {
+        Console.Write($"M = {M}; N = {N} -> \"");
+        NaturalNumbers(M, N);
+        Console.WriteLine("\"");
+    }
+    else
+    {
+        Console.WriteLine("Не коректный ввод, число M ненатуральное.");
+    }
+}
+else
+{
+    Console.WriteLine("Не коректный ввод, число N ненатуральное.");
+}
